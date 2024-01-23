3704ccdd6e30bab84947c6ddb30560c564b1a67d
mkurmanbekov <gapusta97@gmail.com>
2024-01-23 21:54:48 +0600

    Initial commit

---
+++ src/main/kotlin/edu/myrza/jibek/JibekApplication.kt
@@ -0,0 +1,11 @@
+package edu.myrza.jibek
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JibekApplication
+
+fun main(args: Array<String>) {
+	runApplication<JibekApplication>(*args)
+}
---
+++ src/test/kotlin/edu/myrza/jibek/JibekApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.myrza.jibek
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JibekApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
